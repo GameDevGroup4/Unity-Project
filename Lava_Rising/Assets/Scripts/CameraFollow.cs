2af8af95379d3a01cab1d82bcf622f053af2b5cd
FengkaiLiu <liu.fengk@northeastern.edu>
2024-10-31 23:32:11 -0400

    create animation for idle, walk and run
    Link the idle and run animation using blend tree in Animator , create walk animation using run .pngs and create Leftshift function to detect the speed in order to change the animation base on speed

---
+++ Lava_Rising/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    [Range(1, 10)] public float smooth;
+
+    private void FixedUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+
+    void Follow()
+    {
+        Vector3 targetPos = target.position + offset;
+        transform.position = targetPos;
+        Vector3 smoothPos = Vector3.Lerp(transform.position, targetPos, smooth * Time.fixedDeltaTime);
+        transform.position = smoothPos;
+    }
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
--- Lava_Rising/Assets/Scripts/Player.cs
+++ Lava_Rising/Assets/Scripts/Player.cs
@@ -5,22 +5,37 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    public float speed = 4f;
+    [SerializeField] public float speed = 2f;
+    [SerializeField] private Transform groundCheckCollider;
     
     private Rigidbody2D rb;
     private float horizontalval;
+    private float runSpeedMutiplier = 2;
     
+    Animator animator;
+
+    private bool isGrounded = false;
+    bool isRunning = false;
     bool facingRight = true;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     
     void Update()
     {
         //Get horizontal value
         horizontalval = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isRunning = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            isRunning = false;
+        }
     }
 
     void FixedUpdate()
@@ -28,9 +43,20 @@ public class Player : MonoBehaviour
         Move(horizontalval);
     }
 
+    void GroundCheck()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckCollider.position, .2f);
+    }
+
     void Move(float dir)
     {
-        float xVal = dir * speed * 100 * Time.deltaTime;
+        float xVal = dir * speed * 100 * Time.fixedDeltaTime;
+        
+        if (isRunning)
+        {
+            xVal *= runSpeedMutiplier;
+        }
+        
         Vector2 targetVelocity = new Vector2(xVal, rb.velocity.y);
         rb.velocity = targetVelocity;
         
@@ -44,5 +70,10 @@ public class Player : MonoBehaviour
             transform.localScale = new Vector3(1, 1, 1);
             facingRight = true;
         }
+        // Debug.Log(rb.velocity.x);
+        
+        // idle 0; walk 4; run 8
+        
+        animator.SetFloat("xVelocity", Mathf.Abs(rb.velocity.x));
     }
 }
